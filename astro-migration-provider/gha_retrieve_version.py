a8d69e73e8d5ba3e93a713964da35ac7a3f2bab6
Chris Hronek <pohek321@gmail.com>
2022-09-26 17:23:41 -0600

    Remove redundant os var for gh actions

--- astro-migration-provider/gha_retrieve_version.py
+++ astro-migration-provider/gha_retrieve_version.py
@@ -1,10 +1,8 @@
 def get_version():
-    import os
     import configparser
     config = configparser.RawConfigParser()
     config.read_file(open(r'setup.cfg'))
     version = config.get('metadata', 'version')
-    os.environ["VERSION"] = version
     return version
 
 if __name__ == '__main__':
