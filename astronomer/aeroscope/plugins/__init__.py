731df863f12224382c110b1db10ef10c8bc981b8
fritz-astronomer <fritz@astronomer.io>
2023-03-23 17:00:47 -0400

    WIP - telescope charts, prettier landing page

--- astronomer/aeroscope/plugins/__init__.py
+++ astronomer/aeroscope/plugins/__init__.py
@@ -4,6 +4,8 @@ import os
 import socket
 import urllib.error
 from contextlib import redirect_stderr, redirect_stdout
+from textwrap import dedent
+from typing import Optional
 
 import requests
 from airflow.configuration import conf
@@ -25,7 +27,39 @@ bp = Blueprint(
 
 class AeroForm(Form):
     organization = StringField("Organization", [validators.Length(min=4, max=25)])
-    presigned_url = StringField("Pre-signed URL (optional)", [validators.URL(), validators.optional()])
+    presigned_url = StringField(
+        "Pre-signed URL (optional)",
+        description=dedent("""The pre-signed URL field is optional and is to be supplied by an Astronomer Representative.
+    If the pre-signed URL is used, the results of the Telescope Report is shipped to Astronomer."""),
+        validators=[validators.URL(), validators.optional()]
+    )
+
+
+def get_aeroscope_report(date: str, organization: str):
+    import io
+    import runpy
+    from urllib.request import urlretrieve
+
+    version = os.getenv("TELESCOPE_REPORT_RELEASE_VERSION", "latest")
+    a = "airflow_report.pyz"
+    if version == "latest":
+        urlretrieve("https://github.com/astronomer/telescope/releases/latest/download/airflow_report.pyz", a)
+    else:
+        try:
+            urlretrieve(
+                f"https://github.com/astronomer/telescope/releases/download/{version}/airflow_report.pyz", a
+            )
+        except urllib.error.HTTPError as e:
+            flash(f"Error finding specified version:{version} -- Reason:{e.reason}")
+    s = io.StringIO()
+    with redirect_stdout(s), redirect_stderr(s):
+        runpy.run_path(a)
+    return {
+        "telescope_version": "aeroscope-latest",
+        "report_date": date,
+        "organization_name": organization,
+        "local": {socket.gethostname(): {"airflow_report": clean_airflow_report_output(s.getvalue())}},
+    }
 
 
 # Creating a flask appbuilder BaseView
@@ -35,48 +69,35 @@ class StarshipAeroscope(AppBuilderBaseView):
     @expose("/", methods=("GET", "POST"))
     def aeroscope(self):
         form = AeroForm(request.form)
-        if request.method == "POST" and form.validate() and request.form["action"] == "Download":
-
-            import io
-            import runpy
-            from urllib.request import urlretrieve
-
-            VERSION = os.getenv("TELESCOPE_REPORT_RELEASE_VERSION", "latest")
-            a = "airflow_report.pyz"
-            if VERSION == "latest":
-                urlretrieve("https://github.com/astronomer/telescope/releases/latest/download/airflow_report.pyz", a)
-            else:
-                try:
-                    urlretrieve(
-                        f"https://github.com/astronomer/telescope/releases/download/{VERSION}/airflow_report.pyz", a
-                    )
-                except urllib.error.HTTPError as e:
-                    flash(f"Error finding specified version:{VERSION} -- Reason:{e.reason}")
-            s = io.StringIO()
-            with redirect_stdout(s), redirect_stderr(s):
-                runpy.run_path(a)
-            date = datetime.datetime.now(datetime.timezone.utc).isoformat()[:10]
-            content = {
-                "telescope_version": "aeroscope-latest",
-                "report_date": date,
-                "organization_name": form.organization.data,
-                "local": {socket.gethostname(): {"airflow_report": clean_airflow_report_output(s.getvalue())}},
-            }
+        date = datetime.datetime.now(datetime.timezone.utc).isoformat()[:10]
+        if request.method == "POST" and form.validate() and request.form["action"] == "Send/Download Report":
+            filename = f"{date}.{form.organization.data}.data.json"
+            content = get_aeroscope_report(
+                date=date,
+                organization=form.organization.data,
+            )
             if len(form.presigned_url.data) > 1:
                 upload = requests.put(form.presigned_url.data, data=json.dumps(content))
                 if upload.ok:
                     flash("Upload successful")
                 else:
                     flash(upload.reason, "error")
-            filename = f"{date}.{form.organization.data}.data.json"
+
             return Response(
                 json.dumps(content),
                 mimetype="application/json",
                 headers={"Content-Disposition": f"attachment;filename={filename}"},
             )
-        elif request.method == "POST" and request.form["action"] == "Back to Airflow":
-            return redirect(conf.get("webserver", "base_url"))
+        elif request.method == "POST" and form.validate() and request.form["action"] == "Show Report":
 
+            return self.render_template(
+                "aeroscope/report.html",
+                form=form,
+                data=get_aeroscope_report(
+                    date=date,
+                    organization=form.organization.data,
+                )
+            )
         else:
             return self.render_template("aeroscope/main.html", form=form)
 
