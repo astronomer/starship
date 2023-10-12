d75b7a1f5f8cdef02920c3fc7ad84c0b2d3f415b
fritz-astronomer <fritz@astronomer.io>
2023-10-12 18:29:12 -0400

    add better versioning, remove pytest-xdist

--- astronomer_starship/__init__.py
+++ astronomer_starship/__init__.py
@@ -1,4 +1,19 @@
-__version__ = "1.1.1"
+import sys
+
+if sys.version_info >= (3, 8):
+    from importlib import metadata as importlib_metadata
+else:
+    import importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+__version__: str = get_version()
 
 
 def get_provider_info():
