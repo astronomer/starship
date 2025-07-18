05636f9dbb5a1f8b7a909f37fce3c54cc551e9b7
Cedrik Neumann <cedrik.neumann@astronomer.io>
2025-07-18 13:19:20 -0600

    dev: add and update example DAGs
    Now we have a DAG for each scenario we want to test.

---
+++ dev/dags/example_log.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+import logging
+
+from airflow.decorators import dag, task
+from airflow.models import Param
+
+logger = logging.getLogger(__name__)
+
+
+@task
+def run(num_lines: int):
+    for i in range(num_lines):
+        logger.info(f"Log line {i}")
+
+
+@dag(
+    schedule="@daily",
+    start_date=datetime(2025, 6, 1),
+    catchup=False,
+    params={
+        "num_lines": Param(
+            1000, type="number", description="How many lines of log to generate."
+        ),
+    },
+    tags=["log"],
+    render_template_as_native_obj=True,
+)
+def example_log():
+    """A DAG with a task which generates a large log file."""
+
+    run(num_lines="{{ params.num_lines }}")
+
+
+example_log()
---
+++ dev/dags/example_params.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from airflow.decorators import dag
+from airflow.models import Param
+from airflow.operators.bash import BashOperator
+
+
+@dag(
+    schedule="@daily",
+    start_date=datetime(2025, 6, 1),
+    catchup=False,
+    params={
+        "example_param": Param(
+            "default_value", type="string", description="An example parameter"
+        ),
+    },
+    tags=["params"],
+)
+def example_params():
+    """A DAG which uses params."""
+
+    BashOperator(
+        task_id="run",
+        bash_command="echo 'example_param={{ params.example_param }}'",
+    )
+
+
+example_params()
